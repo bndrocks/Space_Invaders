21058a3d2229f064ce83328793642b11f45b50f3
Bennett <bndrocks2012@gmail.com>
2020-05-26 20:35:54 -0300

    Initial

---
+++ Assets/Scriptos/bloco.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bloco : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+    public BoxCollider2D collider;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        rigidbody.constraints = new RigidbodyConstraints2D();
+        collider.isTrigger = true;
+    }
+}
---
+++ Assets/Scriptos/jogador.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jogador : MonoBehaviour
+{
+    public float Velocidade;
+    private float constante = 0.7F;
+    public float HorizontalAxis;
+    public Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        HorizontalAxis = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(constante * Velocidade * HorizontalAxis, 0);
+    }
+}
